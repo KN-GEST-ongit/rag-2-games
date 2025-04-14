ed6dd8c7691204c5b61aa859fbbd2b464c885438
Paweł Buczek <pbuczek7@wp.pl>
2025-04-14 19:45:10 +0200

    feat: initial structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './home/home.page.component';
+import { GamePageComponent } from './game/game.page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    title: 'Home Page',
+  },
+  {
+    path: 'home',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  {
+    path: 'game/:gameName',
+    component: GamePageComponent,
+    title: 'Game Page',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
