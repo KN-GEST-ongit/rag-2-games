fa53c5634a8740e6ec557e5c9b466435e8b293f8
student <d3stud@kia.prz.edu.pl>
2025-04-26 22:47:42 +0200

    feat: #10 added player and moving logic

--- projects/rag-2-games-lib/src/lib/games/snake/models/snake.class.ts
+++ projects/rag-2-games-lib/src/lib/games/snake/models/snake.class.ts
@@ -3,16 +3,84 @@ import { Game } from '../../../models/game.class';
 import { Player } from '../../../models/player.class';
 
 export class SnakeState implements TGameState {
-    // to implement
+    public score = 0;
+    public playerX = 485;
+    public playerY = 285;
+    public isGameStarted = false;
+    // public foodItems = Array.from({ length: 10 }, () => ({
+    //     x: Math.random() * 800,
+    //     y: Math.random() * 600,
+    //   }));
+    public playerSpeedX = 0;
+    public playerSpeedY = 0;
 }
 
 export class Snake extends Game {
     public override name = 'snake';
     public override state = new SnakeState();
 
-    public override outputSpec = ``;
+    public override outputSpec = `
+        output:
+            playerX: float, <0, 980>;
+            playerY: float, <0, 580>;
+            playerSpeedX: float, <-5, 5>;
+            playerSpeedY: float, <-5, 5>;
+            isGameStarted: boolean;
+            score: int, <0, inf>;
+        
+        default values:
+            playerX: 485;
+            playerY: 285;
+            playerSpeedX: 0;
+            playerSpeedY: 0;
+            isGameStarted: false;
+            score: 0;
+
+        const values:
+            playerWidth: 30;
+            playerHeight: 30;
+    `;
     public override players = [
-        //to implement
-        //new Player()
+        new Player(
+            0,
+            true,
+            'Player 1',
+            { moveY: 0, moveX: 0 },
+            {
+                ArrowUp: {
+                    variableName: 'moveY',
+                    pressedValue: -1,
+                    releasedValue: 0,
+                },
+                ArrowDown: {
+                    variableName: 'moveY',
+                    pressedValue: 1,
+                    releasedValue: 0,
+                },
+                ArrowLeft: {
+                    variableName: 'moveX',
+                    pressedValue: -1,
+                    releasedValue: 0,
+                },
+                ArrowRight: {
+                    variableName: 'moveX',
+                    pressedValue: 1,
+                    releasedValue: 0,
+                },
+                ' ': {
+                    variableName: 'start',
+                    pressedValue: 1,
+                    releasedValue: 0,
+                },
+            },
+            '<moveX>: value of {-1, 0, 1}, -1: left, 0: stop, 1: right; <moveY>: value of {-1, 0, 1}, -1: up, 0: stop, 1: down; <start>: value of {0, 1}, 0: not pressed, 1: pressed;',
+            {
+                up: '[ARROW_UP]',
+                down: '[ARROW_DOWN]',
+                left: '[ARROW_LEFT]',
+                right: '[ARROW_RIGHT]',
+                start: '[SPACE]',
+            }
+        ),
     ];
 }
--- projects/rag-2-games-lib/src/lib/games/snake/snake.component.ts
+++ projects/rag-2-games-lib/src/lib/games/snake/snake.component.ts
@@ -8,6 +8,9 @@ import { Snake, SnakeState } from './models/snake.class';
   standalone: true,
   imports: [CanvasComponent],
   template: `
+    <div>
+      score: <b>{{ game.state.score }}</b>
+    </div>
     <app-canvas
       [displayMode]="'horizontal'"
       #gameCanvas></app-canvas>
@@ -17,6 +20,13 @@ export class SnakeGameWindowComponent
   extends BaseGameWindowComponent
   implements OnInit, AfterViewInit, OnDestroy
 {
+  private _playerWidth = 30;
+  private _playerHeight = 30;
+  private _playerXMove = 2;
+  private _playerYMove = 2;
+  private _foodWidth = 10;
+  private _foodHeight = 10;
+
   public override game!: Snake;
 
   public override ngOnInit(): void {
@@ -26,22 +36,94 @@ export class SnakeGameWindowComponent
 
   public override ngAfterViewInit(): void {
     super.ngAfterViewInit();
+    this.resetGame();
     this.render();
   }
 
   public override restart(): void {
     this.game.state = new SnakeState();
+
+    this.game.state.isGameStarted = false;
+    this.resetGame();
   }
 
   protected override update(): void {
     super.update();
+
+    this.gameStart();
+    this.updatePlayerSpeed();
+    if (!this.isPaused && this.game.state.isGameStarted) {
+      this.updatePlayerPosition();
+    }
+
     this.render();
+  }
+
+  private resetGame(): void {
+    this.game.state.playerX = this._canvas.width / 2 - this._playerWidth / 2;
+    this.game.state.playerY = this._canvas.height / 2 - this._playerHeight / 2;
+    this.game.state.score = 0;
+    this.game.state.isGameStarted = false;
+    this.game.state.playerSpeedY = 0;
+    this.game.state.playerSpeedX = 0;
+  }
+
+  private gameStart(): void {
+    if (!this.game.state.isGameStarted && this.game.players[0].inputData['start'] === 1) {
+      this.game.state.isGameStarted = true;
+    }
+  }
+
+  private updatePlayerSpeed(): void {
+    if (this.game.players[0].inputData['moveX'] === -1) {
+      this.game.state.playerSpeedX = -this._playerXMove;
+    }
+    if (this.game.players[0].inputData['moveX'] === 1) {
+      this.game.state.playerSpeedX = this._playerXMove;
+    }
+    if (this.game.players[0].inputData['moveX'] === 0) {
+      this.game.state.playerSpeedX = 0;
+    }
+    if (this.game.players[0].inputData['moveY'] === -1) {
+      this.game.state.playerSpeedY = -this._playerYMove;
+    }
+    if (this.game.players[0].inputData['moveY'] === 1) {
+      this.game.state.playerSpeedY = this._playerYMove;
+    }
+    if (this.game.players[0].inputData['moveY'] === 0) {
+      this.game.state.playerSpeedY = 0;
+    }
+  }
+
+  private updatePlayerPosition(): void {
+    this.game.state.playerX += this.game.state.playerSpeedX;
+
+    this.game.state.playerY += this.game.state.playerSpeedY;
   }
 
   private render(): void {
     const context = this._canvas.getContext('2d');
     if (context) {
-      // Render view here
+      context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+
+      context.fillStyle = 'red';
+      context.fillRect(
+        this.game.state.playerX,
+        this.game.state.playerY,
+        this._playerWidth,
+        this._playerHeight
+      );
+
+      // context.fillStyle = 'green';
+      // this.game.state.foodItems.forEach((food) => {
+      //   context.fillRect(
+      //     food.x,
+      //     food.y,
+      //     this._foodWidth,
+      //     this._foodHeight
+      //   );
+      // }
+      // );
     }
   }
 
