a48794df6c02fd25a5549bd5a7921510e5a838f0
student <d3stud@kia.prz.edu.pl>
2025-05-17 16:57:04 +0200

    feat: #13 added player movement

--- projects/rag-2-games-lib/src/lib/games/spaceinvaders/models/spaceinvaders.class.ts
+++ projects/rag-2-games-lib/src/lib/games/spaceinvaders/models/spaceinvaders.class.ts
@@ -3,16 +3,65 @@ import { Game } from '../../../models/game.class';
 import { Player } from '../../../models/player.class';
 
 export class SpaceinvadersState implements TGameState {
-    // to implement
+    public playerX = 300;
+    public playerSpeed = 5;
+    public laserY = -1;
+    public laserSpeed = 10;
+
+    public aliens = Array.from({ length: 5 }, (_, i) => ({
+        x: 100 + i * 80,
+        y: 50,
+        alive: true,
+    }));
+
+    public alienDirection = 1;
+    public alienSpeed = 2;
+    public score = 0;
+    public isGameStarted = false;
+    public failCounter = 0;
 }
 
 export class Spaceinvaders extends Game {
     public override name = 'spaceinvaders';
     public override state = new SpaceinvadersState();
 
-    public override outputSpec = ``;
+    public override outputSpec = `
+    output:
+        playerX: int, <0, 600>;
+        playerSpeed: int, <1, 10>;
+        laserY: int, <-1, 600>; # -1 means no laser
+        laserSpeed: int, <1, 20>;
+        aliens: [{x: int, <0, 600>, y: int, <0, 400>, alive: boolean}];
+        alienDirection: int, {-1, 1};
+        alienSpeed: int, <1, 5>;
+        score: int, <0, inf>;
+        isGameStarted: boolean;
+        failCounter: int, <0, inf>;
+
+        default values:
+        playerX: 300;
+        playerSpeed: 5;
+        laserY: -1;
+        laserSpeed: 10;
+        alienDirection: 1;
+        alienSpeed: 2;
+        score: 0;
+        isGameStarted: true;
+        failCounter: 0;
+    `;
     public override players = [
-        //to implement
-        //new Player()
+        new Player(
+            0,
+            true,
+            'Player 1',
+            { left: 0, right: 0, shoot: 0 },
+            {
+                a: { variableName: 'move', pressedValue: -1, releasedValue: 0 },
+                d: { variableName: 'move', pressedValue: 1, releasedValue: 0 },
+                ' ': { variableName: 'shoot', pressedValue: 1, releasedValue: 0 },
+            },
+            '<move>: value of {-1, 0, 1}, -1: left, 0: stop, 1: right; <shoot>: value of {0, 1}, 0: not shoot, 1: shoot',
+            { left: '[A]', right: '[D]', shoot: '[SPACE]' }
+        ),
     ];
 }
--- projects/rag-2-games-lib/src/lib/games/spaceinvaders/spaceinvaders.component.ts
+++ projects/rag-2-games-lib/src/lib/games/spaceinvaders/spaceinvaders.component.ts
@@ -8,6 +8,9 @@ import { Spaceinvaders, SpaceinvadersState } from './models/spaceinvaders.class'
   standalone: true,
   imports: [CanvasComponent],
   template: `
+    <div>
+      score: <b>{{ game.state.score }}</b>
+    </div>
     <app-canvas
       [displayMode]="'horizontal'"
       #gameCanvas></app-canvas>
@@ -18,6 +21,11 @@ export class SpaceinvadersGameWindowComponent
   implements OnInit, AfterViewInit, OnDestroy
 {
   public override game!: Spaceinvaders;
+  private _playerWidth = 40;
+  private _playerHeight = 20;
+  private _alienSize = 30;
+  private _laserWidth = 4;
+  private _laserHeight = 15;
 
   public override ngOnInit(): void {
     super.ngOnInit();
@@ -35,15 +43,42 @@ export class SpaceinvadersGameWindowComponent
 
   protected override update(): void {
     super.update();
+
+    this.handleInput();
+
     this.render();
+  }
+
+   private handleInput(): void {
+    const input = this.game.players[0].inputData;
+
+    if (input['move'] === -1) this.game.state.playerX -= this.game.state.playerSpeed;
+    if (input['move'] === 1) this.game.state.playerX += this.game.state.playerSpeed;
+
+    const canvasWidth = this._canvas.width;
+    this.game.state.playerX = Math.max(
+      0,
+      Math.min(canvasWidth - this._playerWidth, this.game.state.playerX)
+    );
+
+    if (input['shoot'] && this.game.state.laserY < 0) {
+      this.game.state.laserY = this._canvas.height - this._playerHeight - this._laserHeight;
+    }
   }
 
   private render(): void {
     const context = this._canvas.getContext('2d');
     if (context) {
-      // Render view here
+      context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+
+      context.fillStyle = 'green';
+      context.fillRect(
+      this.game.state.playerX,
+      this._canvas.height - this._playerHeight,
+      this._playerWidth,
+      this._playerHeight
+      );
     }
   }
 
-  //
 }
