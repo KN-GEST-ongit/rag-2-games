84ec0bbb4b2c6a0f9a76e85ae138f87032741eb7
IgnacyJ <ignacy.janus00@gmail.com>
2026-05-20 13:24:37 +0200

    feat: #63 add info panel and tune super radius, narrowed spawn angles

--- projects/rag-2-games-lib/src/lib/data/games.ts
+++ projects/rag-2-games-lib/src/lib/data/games.ts
@@ -11,7 +11,7 @@ import { Abalone } from '../games/abalone/models/abalone.class';
 import { Tetris } from '../games/tetris/models/tetris.class';
 import { CrossyRoad } from '../games/crossyroad/models/crossyroad.class';
 import { Soccer } from '../games/soccer/models/soccer.class';
-
+import { Crashball } from '../games/crashball/models/crashball.class';
 export const games: Record<string, Game> = {
   pong: new Pong(),
   skijump: new SkiJump(),
@@ -25,4 +25,5 @@ export const games: Record<string, Game> = {
   tetris: new Tetris(),
   crossyroad: new CrossyRoad(),
   soccer: new Soccer(),
+  crashball: new Crashball(),
 };
